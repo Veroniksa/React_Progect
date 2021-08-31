8cf668bac64c0fe1f1e916dc7011a911b4844d63
Veronika <desatericenkoveronika@gmail.com>
2021-08-31 21:24:25 +0200

    les2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import './App.css';
 import { Message } from './components/Message';
 import "./components/Message.css";
 import {Counter} from './components/Counter';
+import { useState } from 'react';
 
 const a = "Vivo in Italia e mangio la pizza... che buona la pizza";
 
 function App() {
+  const [showCounter, setShowCounte] = useState(true);
+
+  const toggleCounter = () => {
+    setShowCounte(!showCounter);
+  }
+
   return (
     <div className="Message">
+      <div onClick={toggleCounter}>Toggle Counter</div>
       <Message name="Leonardo" age={26} text={a} />
-      <Counter />
+      {showCounter && <Counter />}
     </div>
   );
 }
--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,30 +1,62 @@
-import React from "react";
-/* export const Counter = () => {
-    let count = 0;
+import React, { useEffect, useState } from "react";
+
+export const Counter =() => {
+/* const countState = useState(0);
+const count = countState[0];
+const setCount = countState[1]; */
+const[count, setCount] = useState(0);
+
+const[string, setString] = useState("");
+
+useEffect(() => {
+    console.log("like did mount");
+}, []);
+
+useEffect(() => {
+    console.log("like did mount + like did update count");
+}, [count]);
+
+useEffect(() => {
+    return () => {
+        console.log("will unmount");
+    };
+}, []); 
+
     return (
         <div>
             <span>{count}</span>
-            <button onClick={()=>count++}>CLICK</button>
+            <button onClick={()=>setCount(count +1)}>CLICK</button>
         </div>
     );
-}; */
+};
 
-export class Counter extends React.Component {
+/* export class Counter extends React.Component {
 constructor(props) {
     super(props);
-
+    console.log("constructor")
     this.state = {
         count: 0,
     };
 }
+componentDidMount(){
+    console.log("component did mount");
+}
+componentDidUpdate(){
+    console.log("component did update");
+}
+componentWillUnmount(){
+    console.log("component did unmount");
+    clearTimeout(this.timeout)
+}
 
     updateCount = () => {
-        this.setState({
+        this.timeout = setTimeout(()=>this.setState({
             count: this.state.count + 1,
-        })
+        }), 4000);
     }
 
     render(){
+        console.log("render");
         return (
             <div>
                 <div>{this.state.count}</div>
@@ -33,3 +65,4 @@ constructor(props) {
         )
     }
 }
+ */
