78479a9e01206db8087d9ea94f3d3c611186bcba
Veronika <desatericenkoveronika@gmail.com>
2021-09-02 12:19:41 +0200

    homework/les2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./components/Message.css";
 import {Counter} from './components/Counter';
 import { useEffect, useState } from 'react';
 import {MessageList} from './components/MessageList';
+import './components/MessageList/MessageList.css';
 
 const a = "Vivo in Italia e mangio la pizza... che buona la pizza";
 
@@ -13,6 +14,8 @@ const requestBot = [
   {id:3, text: "Jast Do It"}
 ];
 
+const mess = {text:"", author:"bot"};
+
 function App() {
 /*   const [showCounter, setShowCounte] = useState(true);
 
@@ -21,20 +24,25 @@ function App() {
   } */
 
 
-  const [value, setValue] = useState("");
+  //const [value, setValue] = useState("");
 
   const [messagesList, setMessagesList] = useState([]);
 
 
+  const[messageAdd, setMessageAdd] = useState(mess);
 
   useEffect(()=> {
-    if(messagesList[messagesList.length - 1]?.author === "Umano"){
-      setMessagesList((prevMessagesList) => [
-        ...prevMessagesList,
-        {text: "I am robo", author:"robo"},
-      ]);
-    }
+   if(messagesList.length){
+     const ansver = {text: "Hello", author: "robot"};
+     if(messagesList[messagesList.length - 1].author !== "robot"){
+      setTimeout(()=>{
+        setMessagesList([...messagesList, ansver]);
+      }, 1000);
+     }
+   }
   },[messagesList]);
+
+
 
   const handelAddMessage = () => {
     setMessagesList((prevMessagesList) => [...prevMessagesList, 
@@ -42,31 +50,30 @@ function App() {
   };
 
   const handelChange = (event) => {
-    setValue(event.target.value);
+    setMessageAdd({...messageAdd, text: event.target.value});
   };
 
-  const handelSubmit = (event) => {
-    setValue(event.target.value);
-  };
+/*   const handelSubmit = (event) => {
+    setMessageAdd(event.target.value);
+    setMessagesList([...MessageList, message.text]);
+  }; */
 
-  const addMessage = (e) => {
-    console.log("Hia clicatto Add");
-    setMessagesList((prevMessagesList) => [...prevMessagesList, 
-      {text: "Message", author: "Umano"}]);
+  const handelSubmit = (e) => {
     e.preventDefault();
+    console.log("Hia clicatto Add");
+    setMessagesList([...messagesList, messageAdd]);
   };
 
   return (
     <div className="MessageList">
       {/* <div onClick={toggleCounter}>Toggle Counter</div> */}
       {/* <Message name="Leonardo" age={26} text={a} /> */}
-      <button onClick={handelAddMessage}>Add Message</button>
+      {/* <button onClick={handelAddMessage}>Add Message</button> */}
       <form className="MessageListForm" onSubmit={handelSubmit}>
-        <input value={value} onChange={handelChange}/>
-        <button onClick={addMessage}>ADD</button>
+        <input className="MessageListInput" value={messageAdd.text} onChange={handelChange}/>
+        <button className="MessageListBTN">ADD</button>
       </form>
-      {messagesList.map((message, i) => <MessageList key={i} 
-      text = {message.text} />)}
+      {messagesList.map((message, i) => <MessageList key={i} text = {message.text} />)}
       {/* {showCounter && <Counter />} */}
     </div>
   );
