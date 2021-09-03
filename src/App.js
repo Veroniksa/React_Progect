dcfa7f5b0ee2de6365ef9e409dc405fd51f839f4
Veronika <desatericenkoveronika@gmail.com>
2021-09-03 19:11:36 +0200

    work in progress

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import './App.css';
 //import { Message } from './components/Message';
 import "./components/Message.css";
 import {Counter} from './components/Counter';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef, useCallback } from 'react';
 import {MessageList} from './components/MessageList';
 import './components/MessageList/MessageList.css';
+import { Chat } from './components/Chat';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-const a = "Vivo in Italia e mangio la pizza... che buona la pizza";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+
+
+
+
+
+const list = [
+  {name: "Leonardo",
+   id: "1"},
+   {name: "Alieno",
+   id: "2"},
+   {name: "Maria",
+   id: "3"},
+];
+
 
 const requestBot = [
   {id:1, text: "I am stupid bot"},
@@ -17,17 +36,19 @@ const requestBot = [
 const mess = {text:"", author:"bot"};
 
 function App() {
-/*   const [showCounter, setShowCounte] = useState(true);
 
-  const toggleCounter = () => {
-    setShowCounte(!showCounter);
-  } */
+  const inputRef = useRef(null);
 
-
-  //const [value, setValue] = useState("");
+  const [listsItem, setListItem] = useState([
+    {name: "Leonardo",
+     id: "1"},
+     {name: "Aiello",
+     id: "2"},
+     {name: "Maria",
+     id: "3"},
+  ]);
 
   const [messagesList, setMessagesList] = useState([]);
-
 
   const[messageAdd, setMessageAdd] = useState(mess);
 
@@ -39,29 +60,26 @@ function App() {
         setMessagesList([...messagesList, ansver]);
       }, 1000);
      }
+     inputRef.current.focus();
    }
   },[messagesList]);
 
-
-
+/* 
   const handelAddMessage = () => {
     setMessagesList((prevMessagesList) => [...prevMessagesList, 
       {text: "Message UMANO", author: "Umano"}]);
-  };
-
-  const handelChange = (event) => {
-    setMessageAdd({...messageAdd, text: event.target.value});
-  };
-
-/*   const handelSubmit = (event) => {
-    setMessageAdd(event.target.value);
-    setMessagesList([...MessageList, message.text]);
   }; */
+
+  const handelChange = useCallback((event) => {
+    setMessageAdd({...messageAdd, text: event.target.value});
+    console.log(messagesList.length);
+  },[messagesList]);
 
   const handelSubmit = (e) => {
     e.preventDefault();
-    console.log("Hia clicatto Add");
     setMessagesList([...messagesList, messageAdd]);
+    setMessageAdd("");
+    inputRef.current.focus();
   };
 
   return (
@@ -69,10 +87,23 @@ function App() {
       {/* <div onClick={toggleCounter}>Toggle Counter</div> */}
       {/* <Message name="Leonardo" age={26} text={a} /> */}
       {/* <button onClick={handelAddMessage}>Add Message</button> */}
-      <form className="MessageListForm" onSubmit={handelSubmit}>
-        <input className="MessageListInput" value={messageAdd.text} onChange={handelChange}/>
-        <button className="MessageListBTN">ADD</button>
-      </form>
+      {listsItem.map((item, id) => <ListItem key={id} className="listItem">{item.name}</ListItem>)}
+      <FormControl 
+      className="MessageListForm" 
+      onSubmit={handelSubmit}>
+        <TextField autoFocus 
+        label="Message"
+        inputRef={inputRef} 
+        className="MessageListInput" 
+        value={messageAdd.text} 
+        onChange={handelChange}/>
+        <Button 
+        variant="outlined" 
+        size="small" 
+        onClick={handelSubmit} 
+        className="MessageListBTN"
+        >ADD</Button>
+      </FormControl>
       {messagesList.map((message, i) => <MessageList key={i} text = {message.text} />)}
       {/* {showCounter && <Counter />} */}
     </div>
